79226c198f1cc4dd11bca7d74afddce463e48bad
Dayvison Lima <limaare@outlook.com>
2023-05-20 18:48:41 -0300

    teste link whats

--- src/components/BackgroundFixed.jsx
+++ src/components/BackgroundFixed.jsx
@@ -4,7 +4,7 @@ import '../styles/backgroundFixed.css';
 function BackgroundFixed() {
   return (
     <div className="background-fixed">
-      <a className="background-title-link" href="https://api.whatsapp.com/send?phone=5584988060515&text=Olá,%20Crislene,%20gostaria%20de%20saber%20mais%20sobre%20as%20consultas.">
+      <a className="background-title-link" href="https://api.whatsapp.com/send?phone=558488060515&text=Olá,%20Crislene,%20gostaria%20de%20saber%20mais%20sobre%20as%20consultas.">
         <h1 className="background-title">
           Sou Crislene Lopes.
           <br />
